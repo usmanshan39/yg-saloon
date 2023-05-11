b513e2879de324d24822fc80064218c73e00609d
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2023-05-11 16:54:18 +0500

    add modal and more

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -1,15 +1,3 @@
-var divisor = document.getElementById("divisor"),
-handle = document.getElementById("handle"),
-slider = document.getElementById("slider");
-
-function moveDivisor() {
-handle.style.left = slider.value + "%";
-divisor.style.width = slider.value + "%";
-}
-
-window.onload = function () {
-moveDivisor();
-};
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop: true,
