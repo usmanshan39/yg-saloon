42849639d0ed16daace47e031c9ba0654813c33c
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2023-05-23 17:13:49 +0500

    update db

--- admin/database/ygsaloon.sql
+++ admin/database/ygsaloon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2023 at 04:53 PM
+-- Generation Time: May 23, 2023 at 02:13 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -34,7 +34,7 @@ CREATE TABLE `appointments` (
   `mobile` varchar(255) NOT NULL,
   `app_date` varchar(255) NOT NULL,
   `app_time` varchar(255) NOT NULL,
-  `cancel` int(11) NOT NULL DEFAULT 0,
+  `status` varchar(255) NOT NULL DEFAULT 'Pending',
   `updated_at` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,12 +42,37 @@ CREATE TABLE `appointments` (
 -- Dumping data for table `appointments`
 --
 
-INSERT INTO `appointments` (`id`, `name`, `email`, `mobile`, `app_date`, `app_time`, `cancel`, `updated_at`) VALUES
-(1, 'app one', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 0, 0),
-(2, 'app one', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 0, 0),
-(3, 'app one', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 0, 0),
-(4, 'app one', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 0, 0),
-(5, 'app one', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 0, 0);
+INSERT INTO `appointments` (`id`, `name`, `email`, `mobile`, `app_date`, `app_time`, `status`, `updated_at`) VALUES
+(1, 'app one', 'one@gmail.com', '12343123', '2023-04-30', '09:11', 'Cancel', 0),
+(2, 'app two', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 'Complete', 0),
+(3, 'app three', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 'Cancel', 0),
+(4, 'app four', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 'Pending', 0),
+(5, 'app five', 'one@gmail.com', '12343123', '2023-05-02', '14:31:38', 'Complete', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `our_blogs`
+--
+
+CREATE TABLE `our_blogs` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `blog_image` varchar(255) NOT NULL,
+  `blog_desc` longtext NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT 1,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `our_blogs`
+--
+
+INSERT INTO `our_blogs` (`id`, `title`, `blog_image`, `blog_desc`, `published`, `updated_at`) VALUES
+(1, 'one', 'facial-ser.jpg', 'What is Lorem Ipsum?', 1, '2023-05-22 07:54:07'),
+(2, 'two', 'foot-spa.jpg', 'What is Lorem Ipsum? Updated\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 1, '2023-05-22 08:04:31'),
+(3, 'third', '../uploads/massage-ser.jpg', 'What is Lorem Ipsum?', 1, '2023-05-22 07:29:41'),
+(7, 'four', '../uploads/makeup.jpg', 'What is Lorem Ipsum? Updated Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 0, '2023-05-22 08:17:47');
 
 --
 -- Indexes for dumped tables
@@ -60,6 +85,12 @@ ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `our_blogs`
+--
+ALTER TABLE `our_blogs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -68,6 +99,12 @@ ALTER TABLE `appointments`
 --
 ALTER TABLE `appointments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `our_blogs`
+--
+ALTER TABLE `our_blogs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
