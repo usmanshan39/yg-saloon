8641f1a6922195b9d8cb66a0af1b22687e4c18bf
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2023-05-29 17:31:58 +0500

    changes

--- admin/login.php
+++ admin/login.php
@@ -29,43 +29,27 @@
 
     <div class="container">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form class="user" id="loginForm">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                name="email" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                name="password" placeholder="Password">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+        <div class="row" style="height: 100vh;">
+            <div class="col-md-6 m-auto">
+                <div class="card p-4 py-5 p-md-5">
+                    <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                     </div>
+                    <form class="user mt-4" id="loginForm">
+                        <div class="form-group">
+                            <input type="email" class="form-control form-control-user" name="email"
+                                aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control form-control-user" name="password"
+                                placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-user btn-block mt-4">
+                            Login
+                        </button>
+                    </form>
                 </div>
-
             </div>
-
         </div>
 
     </div>
